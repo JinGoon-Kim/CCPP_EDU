2661e76b8f919ad2a31dcc6bfe13a6668b0f33b7
JinGoon-Kim <raneata@gmail.com>
2021-04-29 18:18:17 +0900

    변수의 선언 및 사용

--- cppgame/helloworld.asm
+++ cppgame/helloworld.asm
@@ -3,6 +3,7 @@
 section .text
 global CMAIN
 CMAIN:
+    mov rbp, rsp; for correct debugging
     ;write your code here
     
     ; 메모를 남길 수 있는 기능 = 주석
@@ -19,10 +20,50 @@ CMAIN:
     
     ; 0b10010101 = 0x95
     
-    PRINT_STRING msg
+    ; 8bit = 1byte
+    ; 16bit = 2byte = 1word
+    ; 32bit = 4byte = 2word = 1dword (double-word)
+    ; 64bit = 8byte = 4word = 1qword (quad-word)
+    
+    ; mov reg1, cst -> cst 를 reg1 에 넣기
+    ; mov reg1, reg2 -> reg2 의 값을 reg1 에 복사
+    ; 오른쪽에서 왼쪽으로 데이터 이동
+    
+    mov eax, 0x1234
+    mov rbx, 0x12345678
+    mov cl, 0xff
+    
+    mov al, 0x00
+    mov rax, rdx
+    
+    ; 메모리 <-> 레지스터
+    
+    
+    ;PRINT_STRING msg
     
     xor rax, rax
     ret
+
+    ; 변수의 선언 및 사용
+    ; 변수는 그냥 데이터를 저장하는 바구니 [ ]
+    ; - 처음에 바구니 사용하겠다 선언 (이름과 크기 지정)
     
+    ; 초기화 된 데이터
+    ; [변수이름] [크기] [초기값]
+    ; [크기] db(1) dw(2) dd(4) dq(8)
 section .data
-    msg db 'Hello World', 0x00
+    a db 0x11   ; [0x11]
+    b dw 0x2222
+    c dd 0x33333333
+    d dq 0x4444444444444444
+
+    ; 초기화 되지 않은 데이터
+    ; [변수이름] [크기] [개수]
+    ; [크기] resb(1) resw(2) resd(4) resq(8)
+section .bss
+    e resb 10
+
+
+;section .data
+;    msg db 'Hello World', 0x00
+
