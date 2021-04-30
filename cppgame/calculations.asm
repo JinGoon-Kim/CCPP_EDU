81f02e84e5aee06c20ad4c6662700935fa368928
JinGoon-Kim <raneata@gmail.com>
2021-04-30 21:13:46 +0900

    Update calculations.asm

--- cppgame/calculations.asm
+++ cppgame/calculations.asm
@@ -18,11 +18,14 @@ CMAIN:
     ; b는 레지스터 or 메모리 or 상수
     ; - 단! a, b 모두 메모리는 X
  
+    ; 빼기 연산
+    ; sub a, b (a = a - b)
+ 
     add al, 1 ; 레지스터 + 상수
     PRINT_DEC 1, al  ; 1+1 = 2
     NEWLINE
     
-    add al, [num] ; 레지스터 + 메모
+    add al, [num] ; 레지스터 + 메모리
     PRINT_DEC 1, al
     NEWLINE
  
@@ -31,7 +34,41 @@ CMAIN:
     PRINT_DEC 1, al
     NEWLINE
     
-    add [num], byte 1
+    add [num], byte 1 ;  메모리 + 상수
+    PRINT_DEC 1, [num]
+    NEWLINE
+    
+    add [num], al ;  메모리 + 레지스터
+    PRINT_DEC 1, [num]
+    NEWLINE
+ 
+    ; 곱하기 연산
+    ; mul reg
+    ; - mul bl => al * bl
+    ; -- 연산 결과를 ax에 저장
+    ; -- mul bx => ax * bx
+    ; - mul ebx => eax * ebx
+    
+    ; ex) 5 * 8 은?
+    mov ax, 0
+    mov al, 5
+    mov bl, 8
+    mul bl
+    PRINT_DEC 2, ax
+    NEWLINE
+    
+    ; 나누기 연산
+    ; div reg
+    ; -- 연산 결과는 al(몫) ah(나머지)
+    
+    ; ex) 100 / 3 은?
+    mov ax, 100
+    mov bl, 3
+    div bl
+    PRINT_DEC 1, al
+    NEWLINE
+    mov al, ah
+    PRINT_DEC 1, al
  
     xor rax, rax
     ret
