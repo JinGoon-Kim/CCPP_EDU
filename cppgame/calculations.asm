f34d2bafc264f1b2cf50700d892971dca246776a
JinGoon-Kim <raneata@gmail.com>
2021-04-30 18:52:07 +0900

    Update calculations.asm

--- cppgame/calculations.asm
+++ cppgame/calculations.asm
@@ -8,6 +8,31 @@ CMAIN:
     GET_DEC 1, al
     GET_DEC 1, num
  
+    ;PRINT_DEC 1, al
+    ;NEWLINE
+    ;PRINT_DEC 1, num
+ 
+    ; 더하기 연산
+    ; add a, b ( a = a + b)
+    ; a는 레지스터 or 메모리
+    ; b는 레지스터 or 메모리 or 상수
+    ; - 단! a, b 모두 메모리는 X
+ 
+    add al, 1 ; 레지스터 + 상수
+    PRINT_DEC 1, al  ; 1+1 = 2
+    NEWLINE
+    
+    add al, [num] ; 레지스터 + 메모
+    PRINT_DEC 1, al
+    NEWLINE
+ 
+    mov bl, 3 ; 레지스터 + 레지스터
+    add al, bl
+    PRINT_DEC 1, al
+    NEWLINE
+    
+    add [num], byte 1
+ 
     xor rax, rax
     ret
 
