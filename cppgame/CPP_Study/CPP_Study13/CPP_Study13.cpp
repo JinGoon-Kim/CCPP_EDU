a5bc78c2721db680c94d496aa04c301ede3d950f
JinGoon-Kim <raneata@gmail.com>
2021-07-01 21:19:23 +0900

    stack Frame

---
+++ cppgame/CPP_Study/CPP_Study13/CPP_Study13.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+void PrintHelloWorld()
+{
+    cout << "Hello World" << endl;
+}
+
+void PrintNumber(int number)
+{
+    cout << "넘겨주신 숫자는 " << number << " 입니다" << endl;
+}
+
+
+int MultiplyBy2(int a)
+{
+    int result = a * 2;
+    return result;
+}
+
+
+int MultiplyBy(int a, int b)
+{
+    return a * b;
+}
+
+// F5
+// F10
+// F11
+
+int main()
+{
+    int a = 3;
+    int b = 5;
+    int result = MultiplyBy(b, a);
+
+    PrintNumber(result);
+    //PrintHelloWorld();
+
+    return 0;
+}
